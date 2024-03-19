7c76e3434eb46b59ee12cbe4242917e49269ed27
Zym0tic <lukerozon@gmail.com>
2024-03-19 01:44:21 -0400

    add phjotos to image section

--- src/data.js
+++ src/data.js
@@ -1,7 +1,30 @@
 import Logo from '../src/svg/PSYCH_WAR WHITE (1).svg';
 //import about photos
-import img1 from '../src/img/about/pw_live.jpg'
+import img1 from '../src/img/about/pw_live.jpg';
 import img2 from '../src/img/about/fuck_genocide.png';
+// import images
+import img4 from '../src/img/slides/IMG_1680.jpg';
+import img5 from '../src/img/slides/IMG_1683.jpg';
+import img6 from '../src/img/slides/IMG_1684.jpg';
+import img7 from '../src/img/slides/IMG_1685.jpg';
+import img8 from '../src/img/slides/IMG_1674 (1).jpg';
+import img9 from '../src/img/slides/IMG_20231115_130310_547.webp';
+import img11 from '../src/img/slides/IMG_20240221_123550_694.jpg';
+import img12 from '../src/img/slides/IMG-20231108-WA0000.jpg';
+import img13 from '../src/img/slides/IMG-20231108-WA0001.jpg';
+import img15 from '../src/img/slides/IMG-20231108-WA0003.jpg';
+import img16 from '../src/img/slides/IMG-20231108-WA0004.jpg';
+import img17 from '../src/img/slides/IMG-20231108-WA0005.jpg';
+import img18 from '../src/img/slides/IMG-20231108-WA0006.jpg';
+import img19 from '../src/img/slides/IMG-20231108-WA0007.jpg';
+import img20 from '../src/img/slides/psycho-war-2.jpg';
+import img21 from '../src/img/slides/psycho-war-3.jpg';
+import img22 from '../src/img/slides/psycho-war-4.jpg';
+import img23 from '../src/img/slides/psycho-war-5.jpg';
+import img24 from '../src/img/slides/psycho-war-6.jpg';
+import img25 from '../src/img/slides/psycho-war-7.jpg';
+import img26 from '../src/img/slides/psycho-war-8.jpg';
+import img27 from '../src/img/slides/psycho-war-9.jpg';
 //import discography photos
 import demo23 from '../src/img/discography/demo23.jpg';
 //import icons
@@ -55,7 +78,72 @@ export const aboutData = {
 };
 
 export const slides = [
-
+{
+  url: img4,
+},
+{
+  url: img5,
+},
+{
+  url: img6,
+},
+{
+  url: img7,
+},
+{
+  url: img8,
+},
+{
+  url: img9,
+},
+{
+  url: img11,
+},
+{
+  url: img12,
+},
+{
+  url: img13,
+},
+{
+  url: img15,
+},
+{
+  url: img16,
+},
+{
+  url: img17,
+},
+{
+  url: img18,
+},
+{
+  url: img19,
+},
+{
+  url: img20,
+},
+{
+  url: img21,
+},
+{
+  url: img22,
+},
+{
+  url: img23,
+},
+{
+  url: img24,
+},
+{
+  url: img25,
+},
+{
+  url: img26,
+},
+{
+  url: img27,
+},
 ];
 
 export const discographyData = {
